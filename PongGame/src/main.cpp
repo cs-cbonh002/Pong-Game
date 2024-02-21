216c148eb132f84fbe92fef6c88c8916034de4f9
cs-cbonh002 <bonham_36@hotmail.com>
2024-02-21 15:08:22 -0500

    Add game ball and paddles.

--- PongGame/src/main.cpp
+++ PongGame/src/main.cpp
@@ -16,6 +16,11 @@ int main()
     {
         BeginDrawing(); 
 
+        // Drawuing
+        DrawCircle(screen_width / 2, screen_height / 2, 20, WHITE); 
+        DrawRectangle(10, (screen_height / 2 - 60), 25, 120, WHITE);
+        DrawRectangle(screen_width - 35, (screen_height / 2 - 60), 25, 120, WHITE); 
+
         EndDrawing(); 
     }
 
